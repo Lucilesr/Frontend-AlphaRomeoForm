2e284fca8c150c9570547b0de2e4dadb27f1dda9
Your name <Your email>
2024-07-19 11:23:02 +0200

    last commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd2e3c2e21ce28d8"; }
+/******/ 		__webpack_require__.h = function() { return "3c35d9d83270833b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/FormConfirmation.js
+++ components/FormConfirmation.js
@@ -5,9 +5,9 @@ function FormConfirmation() {
     <div className={styles.confirmationContainer}>
       <div className={styles.confirmContent}>
         <p className={styles.confirmText}>Votre réservation a bien été prise en compte.</p>
-        <p className={styles.confirmText}>Vous serez contacté dans <u>un délai de 48 heures</u>.</p>
+        <p className={styles.confirmText}>Vous serez contacté dans <u>un délai de 48 heures</u>.</p><br></br><br></br>
+        <p>L'équipe Alfa Romeo vous remercie.</p>
       </div>
-      <p className={styles.signatureText}>L'équipe Alfa Romeo vous remercie.</p>
     </div>
   );
 }
